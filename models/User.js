90650036e124c46f714a811a98d9875e3c43f08b
neelesh-yadav <89978881+neelesh-yadav@users.noreply.github.com>
2026-02-02 18:26:11 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ['PMO', 'PM'],
+    required: true
+  },
+  forcePasswordReset: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
 
