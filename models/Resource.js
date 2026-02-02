47e92b77aa44e51d5fc86adf8a52687ee0e9b6e5
neelesh-yadav <89978881+neelesh-yadav@users.noreply.github.com>
2026-02-02 18:29:55 +0530

    Update Resource.js

--- models/Resource.js
+++ models/Resource.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const ResourceSchema = new mongoose.Schema({
+  name: String,
+  weeklyCapacity: Number,
+  allocatedHours: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Resource', ResourceSchema);
 
