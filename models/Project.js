bbe0085fe004027cf00fc57441518c902bc2ddd3
neelesh-yadav <89978881+neelesh-yadav@users.noreply.github.com>
2026-02-02 18:29:01 +0530

    Update Project.js

--- models/Project.js
+++ models/Project.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
 
+const ProjectSchema = new mongoose.Schema({
+  caseId: String,
+  name: String,
+  pm: String,
+  priority: String,
+  type: String,
+  branch: String,
+  budgetCrores: Number,
+  startDate: Date,
+  endDate: Date,
+  status: {
+    type: String,
+    default: 'Pending'
+  },
+  plannedProgress: { type: Number, default: 0 },
+  actualProgress: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
