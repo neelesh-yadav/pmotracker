39c3f2665356a7686742937e6063ac464c2065ba
neelesh-yadav <89978881+neelesh-yadav@users.noreply.github.com>
2026-02-02 18:30:44 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Token missing' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
 
